05113cafa86874aa49d3c742c1a108e77292fafc
rcardin <riccardo.cardin@gmail.com>
2024-01-17 21:46:46 +0100

    Small refactorings

--- app/src/main/kotlin/in/rcard/context/receivers/validation/validation.kt
+++ app/src/main/kotlin/in/rcard/context/receivers/validation/validation.kt
@@ -1,18 +1,40 @@
 package `in`.rcard.context.receivers.validation
 
 import arrow.core.EitherNel
+import arrow.core.raise.either
 
-interface Validatable<T> {
-    fun validate(): EitherNel<ValidationError, T>
+interface Validator<T> {
+    fun validate(toValidate: T): EitherNel<ValidationError, T>
 }
 
 interface ValidationError
 
-data class CreatePortfolioDTO(val userId: String, val amount: Double) : Validatable<CreatePortfolioDTO> {
-    override fun validate(): EitherNel<ValidationError, CreatePortfolioDTO> = TODO()
-}
+data class CreatePortfolioDTO(val userId: String, val amount: Double)
+//    : Validatable<CreatePortfolioDTO> {
+//    override fun validate(): EitherNel<ValidationError, CreatePortfolioDTO> = TODO()
+// }
 
-fun <T : Validatable<T>> validate(validatable: T): EitherNel<ValidationError, T> = validatable.validate()
+val createPortfolioDTOValidator =
+    object : Validator<CreatePortfolioDTO> {
+        override fun validate(toValidate: CreatePortfolioDTO): EitherNel<ValidationError, CreatePortfolioDTO> {
+            // validation logic here
+            TODO()
+        }
+    }
+
+// fun <T : Validatable<T>> process(validatable: T) =
+//    either {
+//        val validated: T = validatable.validate().bind()
+//        // Do something with the validated object
+//    }
+
+fun <T> process(
+    toValidate: T,
+    validator: Validator<T>,
+) = either {
+    val validated: T = validator.validate(toValidate).bind()
+    // Do something with the validated object
+}
 
 fun myMain() {
 }
